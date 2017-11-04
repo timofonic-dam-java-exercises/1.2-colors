7625439f5f08a7f01b2a7143d0a117c30c49a4da
timofonic <timofonic@gmail.com>
2017-11-04 18:16:50 +0100

    Improved JavaDoc comments, fix package name, fix public class name and formatting

---
+++ src/com/timofonic/colors/Colors.java
@@ -0,0 +1,26 @@
+package com.timofonic.colors;
+
+/**
+ * Text colouring:
+ * It shows some words colored as their physical counterparts.
+ * @author Timofonic
+ */
+public class Colors {
+	
+	public static void main(String[] args) {
+
+		// Define the color codes
+		String red = "\033[31m";
+		String green = "\033[32m";
+		String orange = "\033[33m";
+		String blue = "\033[34m";
+		String purple = "\033[35m";
+		String white = "\033[37m";
+		
+		// Print the colored text in screen
+		System.out.print(orange + "tangerine" + green + " herb");
+		System.out.print(orange + " grasshopper" + red + " tomato");
+		System.out.print(white + " sheets" + blue + " sky");
+		System.out.print(purple + " Nazarene" + blue + " sea");
+	}
+}
